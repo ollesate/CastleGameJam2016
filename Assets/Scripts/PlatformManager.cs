1d8a36539c91e59444d52c3f7427b0ce4854933c
Haj <flummeltrollet@gmail.com>
2016-07-05 14:03:47 +0000

    change scripts to use coordinate switches with camera

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -5,7 +5,6 @@ public class PlatformManager : MonoBehaviour {
 
 	GameObject[] platformArray;
 	public bool enabled3d = true;
-	public bool changeDimensions = false;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +15,20 @@ public class PlatformManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(changeDimensions) {
-			enabled3d = !enabled3d;
-			changeDimensions = false;
-			foreach (GameObject platform in platformArray) {
-				platform.GetComponent<PlatformScript>().ChangeDimension(enabled3d);
-			}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			ChangeDimensions (true);
+			enabled3d = true;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			ChangeDimensions (false);
+			enabled3d = false;
+		}
+	}
+
+	void ChangeDimensions (bool is3d) {
+		enabled3d = is3d;
+		foreach (GameObject platform in platformArray) {
+			platform.GetComponent<PlatformScript>().ChangeDimension(is3d);
 		}
 	}
 
