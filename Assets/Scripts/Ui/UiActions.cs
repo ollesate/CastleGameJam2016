0b0fdb480498c3549ba21c6e8ae258ccfa8005c4
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-05 17:12:36 +0200

    Added a main menu with a play button

---
+++ Assets/Scripts/Ui/UiActions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UiActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame() {
+        Debug.Log("Start game");
+        SceneManager.LoadScene("scene");
+    }
+}
