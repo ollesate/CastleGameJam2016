b7083dd9cdac462f102f345cd2f1f201cf73fc82
Haj <flummeltrollet@gmail.com>
2016-07-05 14:53:26 +0000

    add some docstring for class and added platform prefab

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+/* GameObject with platformscript should have the tag "platform"
+ * It needs at least 3 box colliders (may be changed?) for it to work.
+ * 
+ * Collider 1: Should be the ordinary 3d size. Should cover the visual representation.
+ * Collider 2: Should cover the whole 3d space, used in 2d mode.
+ * Collider 3: Should be smaller Y size than collider 2, used to detect if player should be killed. Should be a trigger.
+ * 
+ */
 public class PlatformScript : MonoBehaviour {
 
 	GameObject platform;
@@ -11,20 +20,17 @@ public class PlatformScript : MonoBehaviour {
 		platform = this.gameObject;
 		platformColliders = platform.GetComponents<BoxCollider>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	} //TODO: Remove update so it's not heavy? Check it up
 
 	public void ChangeDimension(bool enabled){
-		if (platformColliders.Length < 2) {
-			Debug.LogError("The GameObject has not enough box colliders, it needs at least 2 to be a legit plattform.");
+		if (platformColliders.Length < 3) {
+			Debug.LogError("The GameObject has not enough box colliders, it needs at least 3 to be a legit plattform.");
 			return;
 		}
-		platformColliders[1].enabled = enabled;
-		//TODO: if enabled && player.x&y is colliding with platform.x&y then player.kill 
+		platformColliders [1].enabled = enabled;
+		platformColliders [2].enabled = enabled;
 	}
+
+		
 
 
 }
