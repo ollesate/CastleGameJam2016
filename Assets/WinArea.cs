1575e5719bc69b00b5ea3acd48a03a61f2ce1110
Maakep <schon.fredrik@gmail.com>
2016-07-05 19:39:50 +0200

    Win area prefab

---
+++ Assets/WinArea.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinArea : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+            Win();
+
+    }
+
+    void Win()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int result;
+        bool isNumber = int.TryParse(scene.name, out result);
+
+        if (isNumber)
+        {
+            try
+            {
+                //you win
+                SceneManager.LoadScene(scene.name + 1);
+            }
+            catch (UnityException e)
+            {
+                //fallback
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+        else
+        {
+            //you win but no next level found
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
