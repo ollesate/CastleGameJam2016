ed6340b6488416c8749ac7da8066c2ec08fede47
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-05 17:21:48 +0200

    Player is taken to MainMenu on death

---
+++ Assets/MainMenuOnDeath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuOnDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy() {
+        Debug.Log("Change scene to main menu");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
