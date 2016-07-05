e5b45412307ae696a5e3c23da99a8e4ec97dc54c
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-05 13:08:26 +0200

    Added a simple scene with a dimension test switch
    Press up arrow to make it 3D, press down arrow to make it 2D.
    The camera will follow the mock player.

---
+++ Assets/Scripts/DimensionSwitchCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DimensionSwitchCamera : MonoBehaviour {
+
+    private Camera cam;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            switchTo2D();
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            switchTo3D();
+        }
+
+	}
+
+    public void switchTo2D() {
+        cam.orthographic = true;
+        Vector3 eulerRot = cam.transform.rotation.eulerAngles;
+        cam.transform.rotation = Quaternion.Euler(new Vector3(0, eulerRot.y, eulerRot.z));
+        Debug.Log("Switching to 2D euler" + eulerRot);
+        
+    }
+
+    public void switchTo3D() {
+        cam.orthographic = false;
+        cam.transform.rotation = initialRotation;
+        Debug.Log("Switching to 3D euler" );
+    }
+}
---
+++ Assets/Scripts/MoveRight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRight : MonoBehaviour {
+
+    public float speed = 1.0f;
+    private float initialZ;
+	// Use this for initialization
+	void Start () {
+        initialZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+        float zSin = Mathf.Sin(Time.time) * 2.0f;
+
+        gameObject.transform.position = new Vector3(pos.x + speed * Time.deltaTime, pos.y, initialZ + zSin);
+	}
+}
