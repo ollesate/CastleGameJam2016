6424396a1d3be6d0f020cfc635f80c12ffa8da09
Haj <flummeltrollet@gmail.com>
2016-07-05 12:40:47 +0000

    Add some files

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformManager : MonoBehaviour {
+
+	GameObject platform;
+	GameObject[] platformArray;
+	BoxCollider platformCollider;
+
+	public bool changeDimensions = false;
+	// Use this for initialization
+	void Start () {
+		//Get all platforms
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(changeDimensions) {
+			changeDimensions = false;
+			foreach (GameObject platform in platformArray) {
+				//platform.GetComponent<PlatformScript>.changeDimensions ();
+			}
+		}
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlattformScript : MonoBehaviour {
+
+	GameObject plattform;
+	BoxCollider plattformCollider;
+
+
+	// Use this for initialization
+	void Start () {
+		plattform = this.gameObject;
+		plattformCollider = plattform.GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} //TODO: Remove update so it's not heavy? Check it up
+
+	void changeDimension(){
+		
+	}
+
+
+}
