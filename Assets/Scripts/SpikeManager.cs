70efec16404ce87e72b599e147902764617487b6
Maakep <schon.fredrik@gmail.com>
2016-07-06 14:11:54 +0200

    Remove a debug log

--- Assets/Scripts/SpikeManager.cs
+++ Assets/Scripts/SpikeManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SpikeManager : MonoBehaviour {
 	public void ChangeDimensions (bool is3d) {
 		GameObject[] spikeArray = GameObject.FindGameObjectsWithTag("spike");
-		Debug.Log ("test: " + spikeArray.Length);
+		
 		foreach (GameObject spike in spikeArray) {
 			if (is3d) {
 				spike.GetComponent<Spikes>().SwitchTo3d ();
