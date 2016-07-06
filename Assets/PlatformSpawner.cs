d34060a16ec9e98881e575b703d66ddef0fbd860
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-06 13:15:00 +0200

    Updated prefabs, adjusting their position, size etc also...
    Added a spawner script. Spawning objects when you press P. The script
    needs to be attached to a gameobject in platform root.

---
+++ Assets/PlatformSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformSpawner : MonoBehaviour {
+
+    public Transform player;
+    public GameObject spawn1;
+    public GameObject[] platforms;
+
+    private float step = 14;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.P)) {
+            SpawnRandom();
+        }
+	}
+
+    public void SpawnRandom() {
+        int randIndex = Random.Range(0, platforms.Length);
+        Spawn(platforms[randIndex]);
+    }
+
+    void Next() {
+        transform.position += transform.right * step;
+    }
+
+    void Spawn(GameObject go) {
+        Next();
+        GameObject spawn = (GameObject)Instantiate(go, transform.position, transform.rotation);
+        spawn.transform.parent = transform.parent;
+    }
+}
