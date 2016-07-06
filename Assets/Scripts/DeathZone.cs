32386de7db051acdf8926b0ec862d3d0ba7e2759
Maakep <schon.fredrik@gmail.com>
2016-07-06 22:49:15 +0200

    delay death 2 sec, make deathzone lethal again

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,13 +5,13 @@ public class DeathZone : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Player") {
-            gameObject.SendMessage("Killed");
+            collision.gameObject.SendMessage("Killed");
         }
     }
 
     void OnTriggerEnter(Collider collider) {
         if (collider.gameObject.tag == "Player") {
-            gameObject.SendMessage("Killed");
+            collision.gameObject.SendMessage("Killed");
         }
     }
 }
