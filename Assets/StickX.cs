3e1195039dce3cf38eaa7dee266064f68b8e6ce5
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-07 00:02:29 +0200

    Spawn torches and background sticks to player x wise

---
+++ Assets/StickX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickX : MonoBehaviour {
+    private float diffX;
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        diffX = target.position.x - transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(target.position.x + diffX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/TorchSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchSpawner : MonoBehaviour {
+
+    public Transform torchesContainer;
+    public GameObject torchPrefab;
+    public Transform playerTransform;
+
+    float requiredDistance = 25;
+
+    float gap = 30;
+
+    float distanceXLeft;
+    float distanceXRight;
+
+	// Use this for initialization
+	void Start () {
+        //distanceXLeft = playerTransform.position.x;
+        distanceXRight = playerTransform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (playerTransform.position.x > distanceXRight - requiredDistance) {
+            SpawnTorchRight();
+        }
+	}
+
+    void SpawnTorchRight() {
+        Debug.Log("Spawned tocrch " + distanceXRight);
+        GameObject go = (GameObject) Instantiate(torchPrefab, new Vector3(distanceXRight, transform.position.y, transform.position.z), transform.rotation);
+        go.transform.parent = torchesContainer;
+        distanceXRight += gap;
+        Debug.Log("Should spawn next " + distanceXRight);
+    }
+
+
+}
