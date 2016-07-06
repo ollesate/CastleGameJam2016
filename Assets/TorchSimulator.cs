6eae2ce735037132bf231e4e03d6b463753a094e
Maakep <schon.fredrik@gmail.com>
2016-07-06 13:29:02 +0200

    torches prefab and script

---
+++ Assets/TorchSimulator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorchSimulator : MonoBehaviour {
+
+    public int startingIntensity = 3;
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Light>().intensity = startingIntensity + (Mathf.Sin(Random.value));
+	}
+}
