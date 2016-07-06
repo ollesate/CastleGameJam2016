e009e9b9cf297b33f1335096379962e7be54bee5
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-06 20:44:31 +0200

    Player should rotate around a corner progres...

--- Assets/MoveForward.cs
+++ Assets/MoveForward.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveRight : MonoBehaviour {
+public class MoveForward : MonoBehaviour {
 
     public float speed = 1.0f;
 
     // Update is called once per frame
     void Update() {
         Vector3 pos = transform.position;
-        gameObject.transform.position += transform.right * speed * Time.deltaTime;
+        gameObject.transform.position += transform.forward * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class DeathZone : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision) {
-        Destroy(collision.gameObject);
-        Debug.Log("OnCollisionEnter");
+        if (collision.gameObject.tag == "Player") {
+            Destroy(collision.gameObject);
+            Debug.Log("OnCollisionEnter");
+        }
     }
 
     void OnTriggerEnter(Collider collider) {
-        Destroy(collider.gameObject);
-        Debug.Log("OnTriggerEnter");
+        if (collider.gameObject.tag == "Player") {
+            Destroy(collider.gameObject);
+            Debug.Log("OnTriggerEnter");
+        }
     }
 }
