746440ada043d9ca02cf669f7acbc5eab025c731
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-06 14:21:08 +0200

    Player now dies when he is below -5

---
+++ Assets/KillWhenBelow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillWhenBelow : MonoBehaviour {
+
+    public float height;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.position.y < height) {
+            Destroy(gameObject);
+        }
+	}
+}
