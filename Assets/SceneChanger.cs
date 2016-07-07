d64caeb6643233d375e38738a5f057b5a04f6290
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-07 16:37:50 +0200

    Shows a popupmenu when you lose instead of changing scene

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void LoadScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+}
