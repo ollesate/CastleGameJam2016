688621f98fa90ebe39b94b0550998ff2d682c57b
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-07 19:36:38 +0200

    Spawn castle in a row

---
+++ Assets/RowSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RowSpawner : MonoBehaviour {
+
+    public float width;
+    public float spawnX;
+    public float spawnWithinXDistance = 100f;
+    public Transform target;
+    private GameObject copyGameObject;
+
+    void Start() {
+        copyGameObject = gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (target.position.x > spawnX - spawnWithinXDistance) {
+            spawnX += width;
+            Vector3 spawnPos = transform.position + transform.right * spawnX;
+            GameObject go = (GameObject)Instantiate(copyGameObject, spawnPos, transform.rotation);
+            go.transform.parent = transform;
+            go.GetComponent<RowSpawner>().enabled = false;
+            copyGameObject = go;
+        }
+	}
+}
