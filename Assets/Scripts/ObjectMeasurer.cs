e43e7c6570f27e6f9603030750e2b0e048fd27bb
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-07 13:09:27 +0200

    Changed platform spawner to be size independent of its patterns

---
+++ Assets/Scripts/ObjectMeasurer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMeasurer : MonoBehaviour {
+    public float width;
+
+	void Awake () {
+        float startX = float.MaxValue;
+        float endX = float.MinValue;
+
+        foreach (Renderer r in GetComponentsInChildren<Renderer>()) {
+            if (endX < r.bounds.max.x) {
+                endX = r.bounds.max.x;
+            }
+
+            if (startX > r.bounds.min.x) {
+                startX = r.bounds.min.x;
+            }
+        }
+        width = endX - startX;
+	}
+}
