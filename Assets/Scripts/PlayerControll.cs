d1f6d40baec1caf9e901105d99e31c36d9a8ec32
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-08 17:14:04 +0200

    Tweeked player movement

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -7,9 +7,9 @@ public class PlayerControll : MonoBehaviour {
 	public float speed = 4.0f;
 	private float xSpeed, zSpeed;
 
-	public float jump = 3.0F;
+	private float jump = 11.0F;
 
-	public float airSpeedAcceleration = 0.1F;
+	private float airSpeedAcceleration = 0.25F;
 
 	public float maxAirSpeed = 3.0F;
 	private float xMaxAirSpeed, zMaxAirSpeed;
@@ -71,19 +71,20 @@ public class PlayerControll : MonoBehaviour {
 					jumpTimer = 0;
 					if (canControlCharacter) {
 						moveDirection.y = jump;
-						justJumped = true;
+                        moveDirection.x *= 1.2f;
+                        justJumped = true;
 					}
 				}
 			} 
 		} else {
-			if (Input.GetButtonUp ("Jump")) {
-				if (controller.velocity.y > 0){
-					Debug.Log ("moveDirection.y before: " + moveDirection.y);
-					moveDirection.y = (float) (controller.velocity.y * 0.5);
-					Debug.Log ("moveDirection.y after: " + moveDirection.y);
-				}
-			}
-			float tempY = moveDirection.y; // probably not needed, haven't tried to fix this (saves it before modification, adds it again after)
+            if (Input.GetButtonUp("Jump")) {
+                if (controller.velocity.y > 0) {
+                    Debug.Log("moveDirection.y before: " + moveDirection.y);
+                    moveDirection.y = (float)(controller.velocity.y * 1f);
+                    Debug.Log("moveDirection.y after: " + moveDirection.y);
+                }
+            }
+            float tempY = moveDirection.y; // probably not needed, haven't tried to fix this (saves it before modification, adds it again after)
 
 			float moveX = moveDirection.x;
 			float moveZ = moveDirection.z;
