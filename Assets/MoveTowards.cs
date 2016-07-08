40c1c4a5bc010e691af3cae92a408ffa7a2817db
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-08 22:44:11 +0200

    Dragon now chases towards player

---
+++ Assets/MoveTowards.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowards : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 4f;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 delta = (target.position - transform.position);
+        if (delta.magnitude < speed * Time.deltaTime) return;
+        transform.position += delta.normalized * speed * Time.deltaTime;
+	}
+}
