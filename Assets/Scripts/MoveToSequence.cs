5c63f209e2a531b6cf685b31cf2b60b4d52a9e13
Olof Sjöholm <olof.sjoholm@gmail.com>
2016-07-09 01:00:44 +0200

    Made a MoveSequence script for camera animations

---
+++ Assets/Scripts/MoveToSequence.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToSequence : MonoBehaviour {
+
+    public Transform[] targetPoints;
+    private bool animComplete;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartSequence());
+	}
+
+    IEnumerator StartSequence() {
+        foreach (Transform t in targetPoints) {
+            StartCoroutine(StartAnimation(t));
+            animComplete = false;
+            yield return new WaitUntil(isAnimationComplete);
+        }
+    }
+
+    bool isAnimationComplete() {
+        return animComplete;
+    }
+
+    IEnumerator StartAnimation(Transform t) {
+        float lerpTime = 1f;
+        float waitTime = 3f;
+        
+        Vector3 startPos = transform.position;
+
+        yield return new WaitForSeconds(waitTime);
+        float startTime = Time.time;
+
+        while (Time.time - startTime < lerpTime) {
+            yield return null;
+            transform.position = Vector3.Lerp(startPos, t.position, (Time.time - startTime) / lerpTime);
+        }
+
+        transform.position = t.position;
+
+        
+        animComplete = true;
+    }
+}
